d6986b26ce16e57d51dbb1f22acbfc9f2e327468
Mikael <krief_mikael@hotmail.com>
2023-08-22 08:09:35 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "resource_group_id" {
   description = "Get the ID of created/updated resource group"
-  value       = ["${azurerm_resource_group.resource_group.id}"]
+  value       = azurerm_resource_group.resource_group.id
 }
 
 output "resource_group_name" {
   description = "Get the name of created/updated Resource group"
-  value       = ["${azurerm_resource_group.resource_group.name}"]
+  value       = azurerm_resource_group.resource_group.name
 }
